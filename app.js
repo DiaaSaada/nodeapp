d141a815100d179f5fb0bf433670b1b592416b60
BORAQ GROUP <diaa@boraq-group.com>
2020-11-29 15:10:55 +0300

    validation, serializeJSON, POST request

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const _ = require('lodash');
 const express = require('express');
 const path = require('path');
+const Joi = require('joi');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use('/public', express.static(path.join(__dirname, 'static')));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
@@ -20,6 +22,15 @@ app.get('/exmaple/:name/:age', (req, res) => {
 
 app.post('/', (req, res) => {
     console.log(req.body)
+    const schema = Joi.object().keys({
+        email: Joi.string().trim().email().required(),
+        password: Joi.string().trim().min(5).max(10).required(),
+    })
+
+
+    const validation = schema.validate(req.body);
+    res.send(validation);
+    console.log(va)
     res.json({ success: true });
 })
 app.listen(3000);
